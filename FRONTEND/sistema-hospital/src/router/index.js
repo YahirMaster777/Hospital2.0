70617d048f79ebf354de65f3b95f13ebe81d8429
YahirMaster777 <210540@utxicotepec.edu.mx>
2024-06-13 13:59:53 -0600

    goooo00000000o0o0o0ool de mexicooooooooooooooooooo

---
+++ FRONTEND/sistema-hospital/src/router/index.js
@@ -0,0 +1,68 @@
+import { createRouter, createWebHistory } from 'vue-router'
+//import HomeView from '../views/HomeView.vue'
+import LoginView from '../components/login.vue'
+import RegisterView from '../components/registerUser.vue'
+import SideBar from '../components/sidebar.vue'
+import Personas from '../components/personas.vue'
+// import Usuarios from '../components/usuarios.vue'
+import Nacimientos from '../components/nacimientos.vue'
+import Seguimiento from '../components/Seguimiento.vue'
+import ListaVacunas from '../components/listaVacunas.vue'
+import Vacunas from '../components/vacunas.vue'
+import Medicos from '../components/datosMedicos.vue'
+import Infante from '../components/datosPaciente.vue'
+import Contacto from '../components/datosContacto.vue'
+
+
+
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: LoginView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      
+      component: LoginView
+    },
+    {
+      path: '/register',
+      name: 'register',
+      
+      component: RegisterView
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: SideBar,
+      children:[
+        {path:'/personas', name:'personas', component:Personas}
+        ,{path:'/nacimientos',name:'nacimientos', component:Nacimientos}
+        ,{path:'/seguimiento',name:'seguimiento', component:Seguimiento}
+        ,{path:'/vacunas',name:'vacunas', component:Vacunas}
+        ,{path:'/listaVacunas',name:'listaVacunas', component:ListaVacunas}
+        ,{path:'/datosContacto',name:'contacto', component: Contacto}
+        ,{path:'/datosPaciente',name:'infante', component: Infante}
+        ,{path:'/datosMedicos',name:'Medicos', component: Medicos}
+
+
+
+      
+      
+      
+      
+      
+      
+      ]
+      
+    }
+  ]
+})
+
+export default router
